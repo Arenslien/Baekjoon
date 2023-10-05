8d414de12a0c7c419d6656d427ab8f70898e6f56
Arenslien <arenslien@gmail.com>
2023-10-05 09:32:17 +0900

    디렉토리 통합

---
+++ 02742.py
@@ -0,0 +1,6 @@
+#BaekJoon 02742
+
+N = int(input()) #정수 N 입력
+
+for i in range(N):
+    print(N-i)
---
+++ factorial_27433.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int factorial(int n) {
+  if (n == 0 || n == 1)
+    return 1;
+  
+  return n * factorial(n-1);
+}
+
+int main(void) {
+  int input;
+
+  scanf("%d", &input);
+
+  printf("%d", factorial(input));
+  
+}
