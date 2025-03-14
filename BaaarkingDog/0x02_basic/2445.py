461b53a6b0e78117a93421606d83b0e3a54e39c5
Arenslien <arenslien@gmail.com>
2025-03-14 14:18:46 +0900

    2445 - python

---
+++ BaaarkingDog/0x02_basic/2445.py
@@ -0,0 +1,16 @@
+# 2445: 별 찍기 - 8
+
+#include <iostream>
+
+N = int(input())
+step = 1
+i = 1
+
+while (i > 0):
+  if (i == N): step = -1
+
+  print("*" * i, end="")
+  print(" " * ((N-i) * 2), end="")
+  print("*" * i)
+
+  i += step
