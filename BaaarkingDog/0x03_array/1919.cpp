8c27ed94ec8172abb2dbd4262c9b6bfd3d091f6d
Arenslien <arenslien@gmail.com>
2025-03-17 14:19:59 +0900

    1919 - c++

---
+++ BaaarkingDog/0x03_array/1919.cpp
@@ -0,0 +1,24 @@
+// 1919: 애너그램
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  int alphabet1[26] = {0}, alphabet2[26] = {0}, cnt = 0;
+  string word1, word2;
+  cin >> word1 >> word2;
+
+  for (int i=0; i<word1.length(); i++) alphabet1[word1.at(i) - 'a']++;
+  for (int i=0; i<word2.length(); i++) alphabet2[word2.at(i) - 'a']++;
+
+  for (int i=0; i<26; i++) {
+    if (alphabet1[i] == alphabet2[i]) continue;
+
+    cnt += (alphabet1[i] > alphabet2[i] ? alphabet1[i]-alphabet2[i]:alphabet2[i]-alphabet1[i]);
+  }
+
+  cout << cnt;
+
+  return 0;
+}
