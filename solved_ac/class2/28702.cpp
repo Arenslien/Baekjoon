a61387c2e0fcabf1d0e005a749d7619136f66b68
Arenslien <arenslien@gmail.com>
2025-12-10 16:27:26 +0900

    28702 - cpp - solved_ac - class2

--- solved_ac/class2/28702.cpp
+++ solved_ac/class2/28702.cpp
@@ -4,23 +4,25 @@
 using namespace std;
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int ans;
 
-  int current = 0;
+    for (int i=3; i>=1; i--) {
+        string input;
+        cin >> input;
 
-  for (int i=0; i<3; i++) {
-    string num;
-    cin >> num;
-
-    if (num != "Fizz" || num != "Buzz" || num != "FizzBuzz") current = stoi(num);
-    current++;
-  }
-
-  if (current%3 == 0 && current%5 == 0) cout << "FizzBuzz";
-  else if (current%3 == 0) cout << "Fizz";
-  else if (current%5 == 0) cout << "Buzz";
-  else current;
-
-  return 0;
+        if (input.back() != 'z') {
+        // if (1 <= input.front()-'0' && input.front()-'0' <= 9) {
+            ans = stoi(input)+i;
+        }
+    }
+    
+    if (ans%3 == 0 && ans%5 == 0) cout << "FizzBuzz";
+    else if (ans%3 == 0) cout << "Fizz";
+    else if (ans%5 == 0) cout << "Buzz";
+    else cout << ans;
+    
+    return 0;
 }
