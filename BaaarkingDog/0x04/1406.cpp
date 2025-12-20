23a10098d95bb8314825778377a9c7f9c84a5798
Arenslien <arenslien@gmail.com>
2025-12-20 23:41:49 +0900

    1406 - cpp - barkingdog - 0x04

--- BaaarkingDog/0x04/1406.cpp
+++ BaaarkingDog/0x04/1406.cpp
@@ -8,26 +8,42 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  list<char> editor;
   string s;
-  int M, cursor;
+  int M;
+
+  // 1. 초기 문자열 s, 명령어 개수 M 입력
   cin >> s >> M;
-  cursor = s.size();
-  
-  for (char c : s) editor.push_back(c);
+
+  // 2. 연결 리스트에 문자열 입력
+  list<char> L;
+  for (char alphabet : s) L.push_back(alphabet);
+
+  list<char>::iterator cursor = L.end();
 
   while (M--) {
-    char cmd, tmp;
+    // 3. cmd 입력
+    char cmd;
     cin >> cmd;
+
+    // 4. cmd 조건
     if (cmd == 'P') {
-      cin >> tmp;
-      editor.push_back(tmp);
-      cursor++;
+      char c;
+      cin >> c;
+      L.insert(cursor, c);
     }
-    else if (cmd == 'L' && cursor > 0) cursor--;
-    // else if (cmd == 'D' && cursor < ) cursor++;
-    else
+    else if (cmd == 'L') { 
+      if (cursor != L.begin()) cursor--;
+    }
+    else if (cmd == 'D') { 
+      if (cursor != L.end()) cursor++;
+    }
+    else { 
+      if (cursor != L.begin()) cursor = L.erase(--cursor);
+    }
   }
+
+  // 5. 출력
+  for (auto l : L) cout << l;
 
   return 0;
 }
