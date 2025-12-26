b679a82bda571515e839ab145a0ddfec55a02be9
Arenslien <46517668+Arenslien@users.noreply.github.com>
2025-12-26 18:22:38 +0900

    23971 - cpp - solved_ac - marathon

--- solved_ac/marathon/23971.cpp
+++ solved_ac/marathon/23971.cpp
@@ -6,32 +6,14 @@
 
 using namespace std;
 
-bool is_prime(int x) {
-  if (x == 1) return true;
-
-  for (int i=2; i*i<=x; i++) {
-    if (x%i == 0) return false;
-  }
-
-  return true;
-}
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-
-  int sum = 0;
-  string word;
-
-  cin >> word;
-
-  for (char c : word) {
-    if ('A' <= c && c <= 'Z') sum += (c - 'A') + 27;
-    else sum += (c - 'a') + 1;
-  }
-
-  if (is_prime(sum)) cout << "It is a prime word.";
-  else cout << "It is not a prime word.";
+  
+  int H, W, N, M;
+  cin >> H >> W >> N >> M;
+  
+  cout << ((H-1)/(N+1)+1)*((W-1)/(M+1)+1);
 
   return 0;
 }
