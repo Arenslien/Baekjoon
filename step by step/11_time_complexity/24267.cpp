b84587133408f0c7c0812608ab20751c49c51b79
Arenslien <arenslien@gmail.com>
2025-12-31 12:33:35 +0900

    24267 - cpp - step_by_step - 11:시간복잡도

--- step by step/11_time_complexity/24267.cpp
+++ step by step/11_time_complexity/24267.cpp
@@ -10,14 +10,27 @@ int main()
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  long long n, cnt = 0;
+  // 첫 번째 풀이
+  // long long n, cnt = 0;
+  // cin >> n;
+
+  // for (long long i = 1; i <= n - 2; i++)
+  // {
+  //   cnt += i * (i + 1) / 2;
+  // }
+  // cout << cnt << '\n' << 3;
+
+  // 두 번째 풀이 --> nCr: 조합 개념
+
+  long long n;
   cin >> n;
 
-  for (long long i = 1; i <= n - 2; i++)
-  {
-    cnt += i * (i + 1) / 2;
-  }
-  cout << cnt << '\n' << 3;
+  // cout << factorial(n)/factorial(3)*(factorial(n-3));
+  cout << n*(n-1)*(n-2)/6 << '\n' << 3;
+
+  // 세 번째 풀이 --> 삼각뿔의 부피: n에 들어갈 값이 n-2임.
+  // i: 1~n-2, j:i+1~n-1, k:j+1~n 이므로 출력값이 최종적으로 nCr과 동일
+  // cout << n*(n+1)*(n+2)/6 << '\n' << 3;
 
   return 0;
 }
