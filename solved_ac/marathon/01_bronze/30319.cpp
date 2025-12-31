02e97502789e92c7643aa6996e52b2d4909e2bfc
Arenslien <arenslien@gmail.com>
2025-12-31 14:33:27 +0900

    30319 - cpp - 푸는 중

---
+++ solved_ac/marathon/01_bronze/30319.cpp
@@ -0,0 +1,24 @@
+// Authored by: Arenslien
+// BOJ: 30319 - Advance to Taoyuan Regional
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string date;
+  cin >> date;
+
+  // 10.21보다 35 이전 "GOOD", 36일 넘으면 "TOO LATE"
+  // 10.1 ~ 10.20: 20일
+  // 9.14~ 9.28: 15일
+  // 9.13~ TOO LATE
+  if (date[5]-)
+
+  return 0;
+}
